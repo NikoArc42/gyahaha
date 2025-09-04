82336b455b2174921c52c4cb93fd7f7cf9d68dc6
NikoArc42 <nikoarc42@gmail.com>
2025-09-04 13:09:18 +0200

    hamgurgertoggle

--- script.js
+++ script.js
@@ -6,4 +6,18 @@ let userLevel = 19;
 let isOnline = true;
 
 console.log("Welcome back" + " " + userName + " " + "Level" + " " + userLevel + " " + "Online" + isOnline);
-console.log("Welcome back" + userName + " Level " + userLevel + " Online " + isOnline);
+console.log("Welcome back " + userName + " Level " + userLevel + " Online " + isOnline);
+
+function hamgurgerToggle() {
+    console.log(hamgurger)
+
+    const navListElement = document.querySelector(".navlist");
+    console.log(navListElement);
+
+    navListElement.Style.display = "flex";
+    navListElement.Style.flexDirection = "column";
+    navListElement.Style.alignitems = "center";
+    navListElement.Style.justifyContent = "center";
+}
+
+globalThis.hamgurgerToggle = hamgurgerToggle;
