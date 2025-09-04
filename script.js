57ba73806f868d8db04be6727886d60a30e1147c
NikoArc42 <nikoarc42@gmail.com>
2025-09-04 11:25:43 +0200

    fix?

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ const userName = "bweh";
 let userLevel = 19;
 let isOnline = true;
 
-console.log("Welcome back" + " " userName + " " + "Level" + " " + userLevel + " " + "Online" + isOnline)
-console.log("Welcome back" + userName + " Level " + userLevel + " Online " + isOnline)
+console.log("Welcome back" + " " userName + " " + "Level" + " " + userLevel + " " + "Online" + isOnline);
+console.log("Welcome back" + userName + " Level " + userLevel + " Online " + isOnline);
